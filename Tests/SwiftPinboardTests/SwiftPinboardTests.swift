fbeb8e0a94e937a9577a85de21d4372a86617d05
Simon Whitaker <sw@netcetera.org>
2023-01-18 16:54:35 +0000

    Fix tests

--- Tests/SwiftPinboardTests/SwiftPinboardTests.swift
+++ Tests/SwiftPinboardTests/SwiftPinboardTests.swift
@@ -5,19 +5,19 @@ import XCTest
 final class SwiftPinboardTests: XCTestCase {
     func testGetURLWithAuthToken() throws {
         let client = PinboardClient(authToken: "foo")
-        let urlString = client.getURLString(path: "/bar", queryArgs: ["baz":"wibble"])
+        let urlString = try? client.getURLString(path: "/bar", queryArgs: ["baz":"wibble"])
         XCTAssertEqual(urlString, "https://api.pinboard.in/v1/bar?auth_token=foo&baz=wibble&format=json")
     }
 
     func testGetURLWithoutAuthToken() throws {
         let client = PinboardClient()
-        let urlString = client.getURLString(path: "/bar", queryArgs: ["baz":"wibble"])
+        let urlString = try? client.getURLString(path: "/bar", queryArgs: ["baz":"wibble"])
         XCTAssertEqual(urlString, "https://api.pinboard.in/v1/bar?baz=wibble&format=json")
     }
 
     func testGetURLWithEncodedQueryArgs() throws {
         let client = PinboardClient()
-        let urlString = client.getURLString(path: "/bar", queryArgs: ["baz":"Wibble Wobble"])
+        let urlString = try? client.getURLString(path: "/bar", queryArgs: ["baz":"Wibble Wobble"])
         XCTAssertEqual(urlString, "https://api.pinboard.in/v1/bar?baz=Wibble%20Wobble&format=json")
     }
 
